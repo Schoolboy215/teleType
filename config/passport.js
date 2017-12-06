86dd1823d36cb33a8032a7d172ce12f048e09041
root <root@coaster.duckdns.org>
2017-12-05 21:08:19 -0500

    initial commit

---
+++ config/passport.js
@@ -0,0 +1,38 @@
+var models = require('../models');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+module.exports = function(passport,config) {
+	passport.use(new GoogleStrategy({
+        	clientID: config.googleClientId,
+        	clientSecret: config.googleClientSecret,
+        	callbackURL: 'http://teletype.personalspaceshow.lawyer:5000/login/callback'
+	},
+        function(token, tokenSecret, profile, done) {
+                process.nextTick(function() {
+                        models.user.findOne({where:{ 'googleId' : profile.id }}).then(function(user) {
+                                if (user) {
+                                        return done(null,user);
+                                } else {
+                                        console.log("not found");
+                                        var newUser = new models.user();
+                                        newUser.googleId = profile.id;
+					newUser.name = profile.displayName;
+                                        newUser.save().then(function(err) {
+                                                if (err)
+                                                        throw err;
+                                                return done(null, newUser);
+                                        });
+					return done(null,null);
+                                }
+
+                        });
+                });
+        })
+);
+passport.serializeUser(function(user, done) {
+        done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+        done(null, user);
+});
+};
